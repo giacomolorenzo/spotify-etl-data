e536ad44ad77f3408eb010364afe4323606215f7
Giacomo Lorenzo <lorenzo.giacomo@hotmail.it>
2021-04-26 14:55:26 +0200

    load in the sqllite database

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import sqlalchemy
+import pandas as pd 
+from sqlalchemy.orm import sessionmaker
 import requests
-import os
-import pandas as pd
+import json
 from datetime import datetime
 import datetime
-from sqlalchemy.orm import sessionmaker
+import sqlite3
 import os
 from dotenv import load_dotenv
 load_dotenv(os.path.join("./", '.env'))
@@ -20,13 +22,13 @@ def check_if_valid_data(df: pd.DataFrame) -> bool:
     if df.isnull().values.any():
         raise Exception("Null valued found")
 
-    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-    yesterday = yesterday.replace(hour=0, minute=0,second=0, microsecond=0)
-    timestamps = df["timestamp"].tolist()
-    for timestamp in timestamps:
-        if datetime.datetime.strptime(timestamp, "%Y-%m-%d") != yesterday:
-            raise Exception("At least one of the returned songs does not come from within the last 24 hours")
-    return True
+    # yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    # yesterday = yesterday.replace(hour=0, minute=0,second=0, microsecond=0)
+    # timestamps = df["timestamp"].tolist()
+    # for timestamp in timestamps:
+    #     if datetime.datetime.strptime(timestamp, "%Y-%m-%d") != yesterday:
+    #         raise Exception("At least one of the returned songs does not come from within the last 24 hours")
+    # return True
 if __name__ == "__main__":
     headers = {
         "Accept": "application/json",
@@ -35,7 +37,7 @@ if __name__ == "__main__":
     }
     print(headers)
     today = datetime.datetime.now()
-    yesterday = today - datetime.timedelta(days=1)
+    yesterday = today - datetime.timedelta(days=60)
     yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
 
 
@@ -54,15 +56,41 @@ if __name__ == "__main__":
         timestamps.append(song["played_at"][0:10])
 
     song_dict = {
-        "song_names" : song_names,
+        "song_name" : song_names,
         "artist_name": artist_names,
         "played_at": played_at_list,
         "timestamp": timestamps
     }
 
-    song_df = pd.DataFrame(song_dict, columns = ["song_names","artist_name","played_at","timestamp"] )
+    song_df = pd.DataFrame(song_dict, columns = ["song_name","artist_name","played_at","timestamp"] )
     
     #Validate
     if check_if_valid_data(song_df):
         print("Data valid, proceed to Load stage")
     
+    #Load
+
+    engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+    conn = sqlite3.connect('my_played_tracks.sqlite')
+    cursor = conn.cursor()
+
+    sql_query = """
+    CREATE TABLE IF NOT EXISTS my_played_tracks(
+        song_name VARCHAR(200),
+        artist_name VARCHAR(200),
+        played_at VARCHAR(200),
+        timestamp VARCHAR(200),
+        CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
+    ) """
+
+    cursor.execute(sql_query)
+    print("Opened database succesfully")
+
+    try:
+        song_df.to_sql("my_played_tracks", engine, index=False, if_exists='append')
+    except:
+        print("Data alredy exists in the database")
+
+    conn.close()
+
+    print("Close database successfully")
