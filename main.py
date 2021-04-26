86b968ff3f4ea0e76771c122edca24742e5d85f7
Giacomo Lorenzo <lorenzo.giacomo@hotmail.it>
2021-04-26 12:00:15 +0200

    add stage and verify

--- main.py
+++ main.py
@@ -8,7 +8,24 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
 TOKEN= os.environ['TOKEN_SPOTIFY']
+def check_if_valid_data(df: pd.DataFrame) -> bool:
+    if df.empty:
+        print("No songs downloaded. Finishing execution")
+        return False
+    if pd.Series(df['played_at']).is_unique:
+       pass 
+    else:
+        raise Exception("Primary Key Check is violated")
+    if df.isnull().values.any():
+        raise Exception("Null valued found")
 
+    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    yesterday = yesterday.replace(hour=0, minute=0,second=0, microsecond=0)
+    timestamps = df["timestamp"].tolist()
+    for timestamp in timestamps:
+        if datetime.datetime.strptime(timestamp, "%Y-%m-%d") != yesterday:
+            raise Exception("At least one of the returned songs does not come from within the last 24 hours")
+    return True
 if __name__ == "__main__":
     headers = {
         "Accept": "application/json",
@@ -17,7 +34,7 @@ if __name__ == "__main__":
     }
     print(headers)
     today = datetime.datetime.now()
-    yesterday = today - datetime.timedelta(days=25)
+    yesterday = today - datetime.timedelta(days=1)
     yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
 
 
@@ -33,14 +50,18 @@ if __name__ == "__main__":
         song_names.append(song["track"]["name"])
         artist_names.append(song["track"]["album"]["artists"][0]["name"])
         played_at_list.append(song["played_at"])
-        timestamps.append(song["played_at"][0:101])
+        timestamps.append(song["played_at"][0:10])
 
     song_dict = {
         "song_names" : song_names,
         "artist_name": artist_names,
-        "played_at_list": played_at_list,
-        "timestamps": timestamps
+        "played_at": played_at_list,
+        "timestamp": timestamps
     }
 
-    song_df = pd.DataFrame(song_dict, columns = ["song_names","artist_name","played_at_list","timestamps"] )
-    print(song_df)
+    song_df = pd.DataFrame(song_dict, columns = ["song_names","artist_name","played_at","timestamp"] )
+    
+    #Validate
+    if check_if_valid_data(song_df):
+        print("Data valid, proceed to Load stage")
+    
